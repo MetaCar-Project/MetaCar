851c8ed45407cc29a1cc30bf1c8c70d582beda46
insung <pp21356@naver.com>
2023-03-09 14:58:28 +0900

    3/9 security 초기작업, DTO변경, 대여 로직작성 - 전인성

--- src/main/java/kosa/metacar/dto/Socar_MemberDTO.java
+++ src/main/java/kosa/metacar/dto/Socar_MemberDTO.java
@@ -6,10 +6,10 @@ import lombok.Data;
 public class Socar_MemberDTO {
 	private String id;
 	private String password;
-	private int phone;
+	private String phone;
 	private String name;
 	//주민번호
-	private int regNum;
+	private String regNum;
 	//총주행거리 (등급위해서)
 	private int totalDistance;
 }
