94ed80dabbb3e28d686847ba7501758f79a0b125
HYC <hanyechan95@gmail.com>
2023-03-14 17:13:04 +0900

    ..

--- src/main/java/kosa/metacar/controller/ReturnController.java
+++ src/main/java/kosa/metacar/controller/ReturnController.java
@@ -1,11 +1,15 @@
 package kosa.metacar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kosa.metacar.dto.Return_CarDTO;
 import kosa.metacar.service.ReturnService;
@@ -22,9 +26,14 @@ public class ReturnController {
 	
 	@PreAuthorize("hasAnyRole('ROLE_USER')")
 	@PostMapping("/return")
-	public String returnCar(Return_CarDTO rc) {
+	@ResponseBody
+	public ResponseEntity<String> returnCar(@RequestBody Return_CarDTO rc) {
+		log.warn("aclksdasacklskslreutnawserfnfnsalfnasdflan" + rc);
+		service.returnCar(rc);
+		String id = rc.getId();
+		log.warn("asfasdfsafsafdsafdsfsdfsafasfas"+ rc);
 		service.returnCar(rc);
 		
-		return "redirect:/main";
+		return new ResponseEntity<>("success",HttpStatus.OK);
 	}
 }
